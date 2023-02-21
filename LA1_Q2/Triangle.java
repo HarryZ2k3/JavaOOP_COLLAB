f77ff5262734643a1bef32d82ecbadf16776e0f6
Harry Phuong Dai's Laptop <phuongdai.saigon@gmail.com>
2023-02-21 14:32:14 +0700

    q2

--- LA1_Q2/Triangle.java
+++ LA1_Q2/Triangle.java
@@ -1,17 +1,42 @@
 class Triangle
 {
-    private double side1;
-    private double side2;
-    private double side3;
+    private double a;
+    private double b;
+    private double c;
     
     public Triangle(double side1, double side2,double side 3)
     {
-        this.side1 = side1;
-        this.side2 = side2;
-        this.side3 = side3;
+        this.a = a;
+        this.b = b;
+        this.c = c;
     }
     public static void TriangleVerification()
     {
-
+        boolean isTriangle = true;
+        //Not triangle
+        if ((a+b)<=c || (a+c)<=b || (b+c)<=a)
+        {
+            System.out.println("*ERROR: THIS IS NOT A TRIANGLE!");
+            isTriangle = false;
+        }
+        if (isTriangle == true)
+        {
+            //Equilateral
+            if (a==b && a==c)
+            {
+                System.out.println("This is a Equlateral triangle!")
+            }
+            //Isoceles
+            if (a=b && a!=c || a=c && a!=b || b=c && b!=a)
+            {
+                System.out.println("This is an Isoceles triangle!");
+            }
+            //Scalene
+            if (a!=b && a!=c && b!=c)
+            {
+                System.out.prinln("This is a Scalene triangle!")
+            }   
+        }
+            
     }
 }
