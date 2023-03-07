68b8b48e92fec2a70d39be675d24f4f6894ccbe3
tquoc0112 <124855237+tquoc0112@users.noreply.github.com>
2023-03-07 14:01:05 +0700

    3rd change

--- LA2_Q1/particle.java
+++ LA2_Q1/particle.java
@@ -1,9 +1,17 @@
-public class particle {
+public class Particle {
     private int x;
+    public int getX() {
+        return x;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
     private int y;
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
 
-    if (x<0)
-        System.out.println("Error:THe box mh")
-
-
-}
+} 
