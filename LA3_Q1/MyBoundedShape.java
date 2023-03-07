b44fa950bc0001b8c36eee3549738d0fa6a809b1
tquoc0112 <124855237+tquoc0112@users.noreply.github.com>
2023-03-07 15:03:45 +0700

    Few changes for LAB3

--- LA3_Q1/MyBoundedShape.java
+++ LA3_Q1/MyBoundedShape.java
@@ -1,6 +1,8 @@
 package LA3_Q1;
 
 public class MyBoundedShape extends MyShape {
+    private int x;
+    private int y;
     public double GetArea(){
         
     }
--- LA3_Q1/MyOval.java
+++ LA3_Q1/MyOval.java
@@ -1,7 +1,23 @@
 package LA3_Q1;
 
 public class MyOval extends MyBoundedShape {
-    public int x;
-    public int y;
 
+    public MyOval(int inX, int inY){
+        super(inX,inY);
+    }
+
+    
+    public int getX(){
+        return x;
+    }
+    public void setX(int x){
+        this.x=x;
+    }
+    
+    public int getY(){
+        return y;
+    }
+    public void setY(int y){
+        this.y=y;
+    }
 }
