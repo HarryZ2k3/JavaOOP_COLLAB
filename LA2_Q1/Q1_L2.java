fe1b73fb1665b9be523505ecace1c7844904d572
Huỳnh Phương Đại <phuongdai.saigon@gmail.com>
2023-03-07 14:24:00 +0700

    small

--- LA2_Q1/Q1_L2.java
+++ LA2_Q1/Q1_L2.java
@@ -1,14 +1,15 @@
-public class Q1_L2
-{
-    public static void main(String[] args)
-    {
-        enum Direction
+enum Direction
         {
             NORTH,
             SOUTH,
             EAST,
             WEST
         }
+public class Q1_L2
+{
+    public static void main(String[] args)
+    {
+        
         
     }
 }
