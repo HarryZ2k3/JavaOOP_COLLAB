569ab512cd6f5ed15019c753521533505b8a30f7
Huỳnh Phương Đại <phuongdai.saigon@gmail.com>
2023-03-07 14:06:28 +0700

    name change

---
+++ LA2_Q1/Q1_L2.java
@@ -0,0 +1,16 @@
+import java.security.spec.ECPrivateKeySpec;
+
+public class Q1_L2
+{
+    public static void main(String[] args)
+    {
+        enum Direction
+        {
+            NORTH,
+            SOUTH,
+            EAST,
+            WEST
+        }
+        
+    }
+}
